f682027396c31425405c3a8e44b1e2b1dcdfd516
Ducvu99 <vuduc112299@gmail.com>
2021-02-07 15:13:09 +0700

    change url 2

--- src/js/app.js
+++ src/js/app.js
@@ -3,12 +3,12 @@ import PicturePuzzle from './PicturePuzzle'
 
 const picturePuzzle = new PicturePuzzle(
   document.querySelectorAll('#puzzle-wrapper > div')[0],
-  'https://scontent.fhan5-1.fna.fbcdn.net/v/t1.0-9/120970626_2115218795278231_5527565250700767042_n.jpg?_nc_cat=109&ccb=2&_nc_sid=09cbfe&_nc_ohc=G5lIfjG_vUwAX_MFXwl&_nc_ht=scontent.fhan5-1.fna&oh=731bb21651bd82ee5710f0704f1b23ca&oe=60434993',
+  '../assets/1.jpg',
   600
 );
 const picturePuzzle2 = new PicturePuzzle(
   document.querySelectorAll('#puzzle-wrapper > div')[1],
-  'https://scontent.fhan5-5.fna.fbcdn.net/v/t1.0-9/102560559_1982338061899639_6642068800551397701_n.jpg?_nc_cat=101&ccb=2&_nc_sid=174925&_nc_ohc=33W9UlzGxuUAX_A8KFw&_nc_ht=scontent.fhan5-5.fna&oh=1b2e52a237c4799aa67355563eeaa306&oe=6045CDBE',
+  '../assets/2.jpg',
   600,
   4
 );
